27f134c723ec833357ac4d6f13ab9e6326d5cf52
vignesh <viignesh7@gmail.com>
2024-03-21 13:31:11 +0530

    updated

---
+++ POLYMORPHISM.py
@@ -0,0 +1,25 @@
+"""Yourself is best example of polymorphism.In front of Your parents You will have one type of
+behaviour and with friends another type of behaviour.Same person but different behaviours at
+different places,which is nothing but polymorphism."""
+
+#operator
+
+a=10
+b=20
+print(a+b) #arithematic operation
+
+a='helo'
+b='haai'
+print(a+b) #string concantiation
+
+#functions
+
+x=[1,2,3,4,5,6]
+
+y={"a":10, True:3 , "c":30, "g":45}
+
+z={1,2,3,4,5} 
+
+print(len(x),len(y),len(z))    # here len function performed differently for different datatype    
+
+#class 
--- datatypes.py
+++ datatypes.py
@@ -2,6 +2,18 @@
 
 list1 = [1, True, 'vicky', 4.90,1]
 
-# tuple (orderd,immutable,aloow duplicate)
+# tuple (orderd,immutable,allow duplicate)
 
-tuple1=("hey",1,False,2.34)
+tuple1=("hey",1,False,2.34,1)
+
+#set (unorderd, dont allow duplicate , unindexed)
+
+set1={1,True, 2.22,"hey"}
+
+# dict (orderd, mutable, dont allow duplicate)
+
+dict1={"a":10,True:10, 1:20 }
+
+# string (indexed,immutable,)
+
+str1="heyyy"
