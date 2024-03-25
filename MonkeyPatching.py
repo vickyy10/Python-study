021f101cd9fabde1707d79dc0322e006334f7859
vignesh <viignesh7@gmail.com>
2024-03-25 10:05:32 +0530

    updated

---
+++ MonkeyPatching.py
@@ -0,0 +1,14 @@
+#if we call multi the add will exicute 
+
+class math:
+    def multi(self,a,b):
+        return a * b
+    
+    def add(self,c,d):
+        return c + d
+    
+math.multi = math.add
+
+obj = math()
+
+print(obj.multi(4,5))
