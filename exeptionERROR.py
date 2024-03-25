ad8cc73caeb15969876e74b1f85246ad95659ea1
vignesh <viignesh7@gmail.com>
2024-03-25 15:31:57 +0530

    updated

---
+++ exeptionERROR.py
@@ -0,0 +1,18 @@
+
+try:
+
+    a =int(input())
+
+    b =int(input())
+
+    c=a/b
+
+    print(c)
+
+except:
+    print("enter valid number")
+else:
+    print(c)
+
+finally:
+    print("must execute5")
