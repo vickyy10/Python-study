22888f1d057fa19750f5098a5de59b3025d09ef1
vignesh <viignesh7@gmail.com>
2024-03-28 11:46:35 +0530

    updated

---
+++ dict_comperhention.py
@@ -0,0 +1,4 @@
+people={'hey':20, 'eh':30, 'yye':199}
+
+new_dict={k.upper():a for (k,a) in people.items()}
+print(new_dict)
---
+++ setComperhention.py
@@ -0,0 +1,5 @@
+set_num={1,2,3,4,5,6,7,1,2,3,4} #not allow duplicate
+
+sqauere_set={i**2 for i in set_num}
+
+print(sqauere_set)
