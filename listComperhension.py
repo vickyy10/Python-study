2b613eb5feef227476d5fac5b0c14aaa54f573ff
vignesh <viignesh7@gmail.com>
2024-03-28 10:28:57 +0530

    updated

--- listComperhension.py
+++ listComperhension.py
@@ -1,5 +1,12 @@
-list_1=['he',10,20,40,'hello','ooi']
+# list_1=['he',10,20,40,'hello','ooi']
 
-new_list1=[item for item in list_1 if isinstance(item, int)]
+# new_list1=[item for item in list_1 if isinstance(item, int)]
 
-print(new_list1)
+# print(new_list1)
+
+num_list=[1,2,3]
+s="abc"
+
+tup_list=[(i,j) for i in num_list for j in s]
+
+print(tup_list)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+list_1=['he',10,20,40,'hello','ooi']
+
+# new_list1=[item for item in list_1 if isinstance(item, int)]
+
+# print(new_list1)
