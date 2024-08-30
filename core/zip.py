f3b2d6f388afbbf2f255c243d0eff8a51305c34a
vignesh <viignesh7@gmail.com>
2024-08-30 10:26:13 +0530

    updated

--- core/zip.py
+++ core/zip.py
@@ -1,7 +1,50 @@
-list1=['vicky','jasil','sunu']
 
-list2=[20,30,40]
+#  two iterator
 
-zipped = dict(zip(list1,list2))
+# list1=['vicky','jasil','sunu']
 
-print(zipped)
+# list2=[20,30,40]
+
+# zipped = dict(zip(list1,list2))
+
+# print(zipped)
+
+
+
+
+
+# three iterator
+
+
+# l=[1,2,3,4,5,6]
+
+# t=("a","b","c","d","e")
+
+# s="heloooooooo"
+
+# d={"x":"xx","y":"yy","z":"zz"}
+
+
+# r=list(zip(l,t,s,d.values()))
+
+# print(r)  
+
+
+
+
+
+
+# unzipping
+
+
+# l=[1,2,3,4,5,6]
+
+# t=("a","b","c","d","e","f")
+
+# r=list(zip(l,t))
+
+# tt,ll=zip(*r)
+# print(r)
+# print(tt)
+# print(ll)
+
